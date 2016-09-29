fc3efb496fb947ec08d06e87b2a6ffce66741c46
Funtaztic <abelszabolcsi@gmail.com>
2016-09-29 20:34:39 +0200

    Fifth commit
    this is the end, enough testing

--- Test_Prog.py
+++ Test_Prog.py
@@ -1,4 +1,8 @@
 #This is just some text.
+<<<<<<< HEAD
 
 print "Hello Git, this is awesome!"
 stop = raw_input()
+=======
+>>>>>>> parent of a2ea7ec... Third commit
+print "Ctrl+Shift+X" #makes a git commit from Atom
